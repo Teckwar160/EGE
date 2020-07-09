428aec0e9cd4360698d6905b2be34235c349242a
AbrahamRH <abraham.esc.tra@gmail.com>
2020-07-09 18:30:51 -0500

    Se agrago la parte de System

---
+++ include/core/System.hpp
@@ -0,0 +1,58 @@
+#ifndef SYSTEM_HPP
+#define SYSTEM_HPP
+
+using SystemType = int;
+
+namespace EGE{
+
+    class SystemBase{
+        public:
+            /**Atributo que identifica el tipo de sistema*/
+            inline static SystemType nextType = 0;
+
+            /**
+             *@brief Método constructor.
+             */
+            SystemBase();
+
+            /**
+             *@brief Método destructor.
+             */
+            virtual ~SystemBase() = 0;
+
+    };
+    
+    /**
+     *@brief Interface de cualquier sistema que se necesite del motor.
+     */
+    template<class Type>
+    class System : public SystemBase{
+        private:
+            SystemType type = 0;
+        public:
+        /**
+         *@brief Método constructor vacío.
+         */
+        System();
+
+        /**
+         *@brief Método constructor.
+         *
+         *@param type identificador del tipo de sistema.
+         */
+        System(SystemType type);
+
+        /**
+         *@brief Método destructor.
+         */
+        virtual ~System() = 0;
+
+        /**
+         *@brief Método que nos regresa el identificador del tipo de sistema.
+         *@return SystemType
+         */
+        static SystemType getSystemType();
+    };
+}
+
+#endif
---
+++ src/core/System.tpp
@@ -0,0 +1,33 @@
+#include "../../include/core/System.hpp"
+
+namespace EGE{
+    template<class Type>
+    System<Type>::System(){}
+
+    template<class Type>
+    System<Type>::System(SystemType type){
+        this -> type = type;
+    }
+
+    template<class Type>
+    System<Type>::~System(){
+        /*Nada*/
+    }
+
+    
+    template<class Type>
+    SystemType System<Type>::getSystemType(){
+
+        static SystemType _type = nextType++;
+
+        return _type;
+    }
+
+    SystemBase::SystemBase(){
+        /*Nada*/
+    }
+
+    SystemBase::~SystemBase(){
+        /*Nada*/
+    }
+}
